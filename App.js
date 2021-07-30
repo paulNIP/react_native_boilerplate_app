46985479d5f796d8161be71443b888afd9bf82c0
Hwang-yoonji <kaci00@naver.com>
2021-07-30 15:19:04 +0900

    CustomHeader

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, SafeAreaView, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+function CustomHeader() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flexDirection: 'row', height: 50, borderWidth: 1, borderColor: 'red'}}>
+      <View style={{flex: 1, justifyContent: 'center', borderColor: 'red', borderWidth: 1}}>
+        <Image style={{width: 30, height: 30, marginLeft: 5}} source={require('./assets/menu.png')} resizeMode="contain" />
+      </View>
+      <View style={{flex: 1.5, justifyContent: 'center', borderColor: 'red', borderWidth: 1}}>
+        <Text style={{textAlign: 'center'}}>Title</Text>
+      </View>
+      <View style={{flex: 1, borderColor: 'red', borderWidth: 1}}></View>
     </View>
+  )
+}
+
+function HomeScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <CustomHeader/>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function SettingScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <CustomHeader/>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Setting!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Setting" component={SettingScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
